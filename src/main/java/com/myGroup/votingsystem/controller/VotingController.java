1f07b449e6e1a83d0729b9502119daf971f41a29
Mark Smart <marksmart26@gmail.com>
2019-08-10 23:11:55 +0100

    adding logger functionality to properties/VotingController

--- src/main/java/com/myGroup/votingsystem/controller/VotingController.java
+++ src/main/java/com/myGroup/votingsystem/controller/VotingController.java
@@ -1,16 +1,13 @@
 package com.myGroup.votingsystem.controller;
 
 import java.util.List;
-
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.myGroup.votingsystem.entity.Candidate;
 import com.myGroup.votingsystem.entity.Citizen;
 import com.myGroup.votingsystem.repositories.CandidateRepo;
@@ -18,6 +15,8 @@ import com.myGroup.votingsystem.repositories.CitizenRepo;
 
 @Controller
 public class VotingController {
+	
+	public final Logger logger = Logger.getLogger(VotingController.class);
 	
 	@Autowired
 	CitizenRepo citizenRepo;
@@ -27,17 +26,21 @@ public class VotingController {
 	
 	@RequestMapping("/")
 	public String goToVote() {
+		logger.info("Returning vote.html file");
 		return "vote.html";
 	}
 	
 	@RequestMapping("/doLogin")
 	public String doLogin(@RequestParam String name, Model model, HttpSession session) {
 		
+		logger.info("getting citizen from database");
 		Citizen citizen = citizenRepo.findByName(name);
 		
+		logger.info("putting citizen into session");
 		session.setAttribute("citizen", citizen);
 		
 		if(!citizen.getHasVoted()) {
+			logger.info("putting candidates into model");
 			List<Candidate> candidates = candidateRepo.findAll();
 			model.addAttribute("candidates", candidates);
 			return "/performVoted.html";
@@ -49,11 +52,13 @@ public class VotingController {
 	
 	@RequestMapping("/voteFor")
 	public String voteFor(@RequestParam Long id, HttpSession session) {
+		
 		Citizen cN = (Citizen)session.getAttribute("citizen");
 		
 		if(!cN.getHasVoted()) {
 			cN.setHasVoted(true);
 			Candidate c = candidateRepo.findById((long)id);
+			logger.info("voting for candidate" +c.getName());
 			c.setNumberOfVotes(c.getNumberOfVotes()+1);
 			candidateRepo.save(c);
 			citizenRepo.save(cN);
